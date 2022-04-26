7fa477090c11e81c73cf91412e2447232f7e9d01
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-04-26 16:14:08 +0200

    attempt 8

--- task3/train.py
+++ task3/train.py
@@ -10,8 +10,8 @@ from glob import glob
 
 from tensorflow.keras.layers import Dense, Input, Flatten
 from tensorflow.keras.models import Model
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import preprocess_input
+from keras.applications.vgg19 import VGG19
 
 
 def get_image_vector(img_path):
@@ -31,12 +31,15 @@ if __name__ == "__main__":
     input_shape = (img_rows, img_cols, 3)
     epochs = 5
 
+    features_file_name = "features_vgg19.pkl"
+
     try:
-        with open('features_vg16.pickle', 'rb') as f:
+        with open(features_file_name, 'rb') as f:
             features_dct = pickle.load(f)
     except FileNotFoundError:
         # Pre-trained model to extract features
-        model = VGG16(include_top=False, input_tensor=Input(shape=input_shape))
+        print("Loading VGG19 model...")
+        model = VGG19(include_top=False, input_tensor=Input(shape=input_shape))
         flat1 = Flatten()(model.layers[-1].output)
         model = Model(inputs=model.inputs, outputs=flat1)
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -59,7 +62,7 @@ if __name__ == "__main__":
         features = model.predict(np.concatenate(list(preprocessed_images.values()), axis=0))
         features_dct = {k: v for k, v in zip(preprocessed_images.keys(), features)}
 
-        with open('features_vg16.pickle', 'wb') as f:
+        with open(features_file_name, 'wb') as f:
             pickle.dump(features_dct, f)
 
     full_rows = []
