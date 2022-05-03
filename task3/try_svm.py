bdb4eb9b2b912c2014477c87b3f6e22ca257da21
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-05-03 21:31:00 +0200

    110

--- task3/try_svm.py
+++ task3/try_svm.py
@@ -40,7 +40,7 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(x_train, y_labels, test_size=0.2, random_state=42)
 
-    steps = [('scaling', StandardScaler()), ('reduce_dim', PCA(n_components=80)), ('clf', SVC())]
+    steps = [('scaling', StandardScaler()), ('reduce_dim', PCA(n_components=110)), ('clf', SVC())]
     pipeline = Pipeline(steps)
 
     print("Fitting the pipeline...")
