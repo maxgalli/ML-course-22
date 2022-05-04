3f408b57df87a9fa30fcc443b17ee21b4244ada0
Massimiliano Galli <massimiliano.galli@cern.ch>
2022-05-04 10:04:40 +0200

    second attempt with knn

--- task3/try_knn.py
+++ task3/try_knn.py
@@ -41,7 +41,7 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(x_train, y_labels, test_size=0.2, random_state=42)
 
-    steps = [('scaling', StandardScaler()), ('reduce_dim', PCA(n_components=80)), ('clf', KNeighborsClassifier(n_neighbors=1))]
+    steps = [('scaling', StandardScaler()), ('reduce_dim', PCA(n_components=80)), ('clf', KNeighborsClassifier(n_neighbors=11))]
     pipeline = Pipeline(steps)
 
     print("Fitting the pipeline...")
